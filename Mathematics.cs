474543f879b09db141ae983dd7a67c6f7c7c1cff
kunturs <72376907+kunturs@users.noreply.github.com>
2020-12-10 10:47:09 +0100

    Add files via upload

---
+++ Mathematics.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_10
+{
+    public static class Functions
+    {
+        public static Func<double, double> Constant (double constantValue)
+        {
+            return (x) => constantValue;
+        }
+
+        public static Func<double, double> Identity()
+        {
+            return (x) => x;
+        }
+
+        public static Func<double, double> Exp(double coefficientValue)
+        {
+            return (x) => Math.Exp(x) * coefficientValue;
+        }
+    }
+
+    public class Function
+    {
+        
+
+        private Func<double, double> internalFunction;
+
+        public Function(Func<double, double> fp)
+        {
+            func = fp;
+        }
+
+        private Func<double, double> func {
+
+            get => internalFunction;
+            set { internalFunction += value; }
+        }
+        public double Value(double d)
+        {
+            return internalFunction(d);
+        }
+
+        public static implicit operator Function(Func<double, double> f)
+        {
+            return new Function(f);
+        }
+
+        public IEnumerable<double> GetValues(double aValue, double bValue, int nValue)
+        {
+            for(int i=0; i<nValue; i++)
+            {
+                yield return internalFunction(aValue + (bValue - aValue) / nValue * i);
+            }
+        }
+    }
+
+    //public class Polynomial : Function
+    //{
+    //    static ToFunction(double[] coefficientValues)
+    //    {
+
+    //    }
+    //}
+
+    public static class NumericalMethod 
+    {
+        public static double Derivative (this Function f, double x)
+        {
+            double h = 0.001;
+            return (f.Value(x+h) - f.Value(x -h))/ (2 * h);
+        }
+
+
+        public static double Integral(this Function f, double x)
+        {
+            double n = 100;
+            //Func<double, double> f1 = Functions.Exp(1);
+            return (f.Value(x) - f.Value(x +1)) / (2 * n);
+        }
+
+        //public static Func<double, double> Integral(this double h)
+        //{
+        //    return (x) => ((x) + (x + 1)) / 2 * h;
+        //    /*Sum of (F(xi) + F(xi+1)) / 2 * h'.*/
+        //}
+
+    }
+
+
+   
+
+}
--- Program.cs
+++ Program.cs
@@ -1,264 +1,83 @@
-﻿#define STAGE1
-#define STAGE2
-//#define STAGE3
-//#define STAGE4
+﻿using System;
 
-using System;
-using System.Globalization;
-
-namespace Fraction
+namespace Lab_10
 {
     class Program
     {
-        private static int TestCounter = 0;
-
-        static void Test(object obj1, object obj2, bool equals = true)
-        {
-            if (obj1.Equals(obj2) == equals)
-                Console.WriteLine($"  {++TestCounter:00}. OK! \"{obj1}\" " + (equals ? "==" : "!=") + $" \"{obj2}\"");
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"  {++TestCounter:00}. Error! \"{obj1}\" == \"{obj2}\" is not {equals}!");
-            }
-            Console.ResetColor();
-        }
-
-        static void TestMsg(string message, bool ok = true)
-        {
-            if (ok)
-                Console.WriteLine($"  {++TestCounter:00}. OK! {message}");
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"  {++TestCounter:00}. Error! {message}");
-            }
-            Console.ResetColor();
-        }
-
         static void Main(string[] args)
         {
-#if STAGE1
-
-            Console.WriteLine("\nSTAGE 1 - Constructors, properties, Reciprocal()");
-
-            Console.WriteLine("\nTest constructors");
-
-            Fraction frac;
-            frac = new Fraction(2);
-            Test($"{frac}", "[2]");
-
-            frac = new Fraction(1, 3);
-            Test($"{frac}", "[1/3]");
-
-            frac = new Fraction(2, 4);
-            Test($"{frac}", "[1/2]");
-
-            frac = new Fraction(-7, 14);
-            Test($"{frac}", "-[1/2]");
-
-            frac = new Fraction(-1, 3);
-            Test($"{frac}", "-[1/3]");
-
-            frac = new Fraction(4, 2);
-            Test($"{frac}", "[2]");
-
-            frac = new Fraction(10, 3);
-            Test($"{frac}", "[3 1/3]");
-
-            frac = new Fraction(-6, 3);
-            Test($"{frac}", "-[2]");
-
-            try
+            Console.WriteLine("-------------------------- Stage_1 (1.0) --------------------------");
             {
-                frac = new Fraction(5, -3);
-                Test($"{frac}", "Should throw exception");
-            }
-            catch (ArgumentException)
-            {
-                TestMsg("Exception has been thrown");
+                Func<double, double> constantFunctionZero = Functions.Constant(0.0f);
+                Func<double, double> constantFunctionOne = Functions.Constant(1.0f);
+
+                Func<double, double> identityFunctionZero = Functions.Identity();
+
+                Func<double, double> exponentialfunctionZero = Functions.Exp(0.0f);
+                Func<double, double> exponentialfunctionOne = Functions.Exp(0.15f);
+
+                {
+                    Console.WriteLine($"Constant Function #0    : {constantFunctionZero(5.0f),2:F} (Should be 0.00)");
+                    Console.WriteLine($"Constant Function #1    : {constantFunctionOne(5.0f),2:F} (Should be 1.00)");
+
+                    Console.WriteLine($"Identity Function #0    : {identityFunctionZero(5.0f),2:F} (Should be 5.00)");
+
+                    Console.WriteLine($"Exponential Function #0 : {exponentialfunctionZero(5.0f),2:F} (Should be 0.00)");
+                    Console.WriteLine($"Exponential Function #1 : {exponentialfunctionOne(5.0f),2:F} (Should be 22.26)");
+                }
             }
 
-            Console.WriteLine("\nTest properties");
-
-            frac = new Fraction(2, 4);
-            Test($"l={frac.Numerator} m={frac.Denominator}", "l=1 m=2");
-
-            frac = new Fraction(6, 3);
-            Test($"l={frac.Numerator} m={frac.Denominator}", "l=2 m=1");
-
-            frac = new Fraction(1, 6);
-            frac.Numerator = 2;
-            Test($"l={frac.Numerator} m={frac.Denominator}", "l=1 m=3");
-            frac.Numerator = 0;
-            Test($"l={frac.Numerator} m={frac.Denominator}", "l=0 m=1");
-
-            frac = new Fraction(-4, 3);
-
-            try
+            Console.WriteLine("-------------------------- Stage_2 (1.0) --------------------------");
             {
-                frac.Denominator = -6;
-                Test($"l={frac.Numerator} m={frac.Denominator}", "Should throw Exception!!!");
-            }
-            catch (ArgumentException)
-            {
-                TestMsg("Exception has been thrown");
+                Function constantFunctionZero = Functions.Constant(1.0f);
+                Function identityFunctionZero = Functions.Identity();
+
+                Function exponentialfunctionZero = new Function(Functions.Exp(1.0f));
+
+                {
+                    Console.WriteLine($"Constant Function #0    : {constantFunctionZero.Value(5.0f),2:F} (Should be 1.00)");
+                    Console.WriteLine($"Identity Function #0    : {identityFunctionZero.Value(5.0f),2:F} (Should be 5.00)");
+
+                    Console.Write($"Exponential Function #0 : ");
+                    foreach (double currentValue in exponentialfunctionZero.GetValues(1.0f, 2.0f, 5))
+                        Console.Write($"{currentValue,2:F} ");
+
+                    Console.WriteLine("(Shoule be 2.72 3.32 4.06 4.95 6.05 7.39)");
+                }
             }
 
-            try
+            Console.WriteLine("-------------------------- Stage_3 (1.5) --------------------------");
+            //{
+            //    Polynomial polynomialFunctionZero = new Polynomial(new double[] { 4.0f, 1.0f, 1.0f });
+            //    Polynomial polynomialFunctionOne = new Polynomial(new double[] { 0.0f, 2.0f });
+
+            //    Console.WriteLine($"Polynomial Function #0   : {polynomialFunctionZero.Value(2.0f),2:F} (Should be 10.00)");
+            //    Console.WriteLine($"Polynomial Function #1   : {polynomialFunctionOne.Value(3.0f),2:F} (Should be 6.00)");
+
+            //    Console.WriteLine($"Polynomial Derivative #0 : {polynomialFunctionOne.Derivative(2.25f),2:F} (Should be 2.00)");
+            //}
+
+            Console.WriteLine("-------------------------- Stage_4 (1.5) --------------------------");
             {
-                frac.Denominator = 0;
-                Test($"l={frac.Numerator} m={frac.Denominator}", "Should throw Exception!!!");
+                Function exponentialfunctionZero = Functions.Exp(1.0f);
+
+                //Polynomial polynomialFunctionZero = new Polynomial(new double[] { 0.0f, 2.0f });
+                //Polynomial polynomialFunctionOne = new Polynomial(new double[] { 4.0f, 1.5f, 1.0f, 7.5f, 12345.6f });
+
+
+
+
+                Console.WriteLine($"Integral Function #0                 : {exponentialfunctionZero.Integral(0.0f, 2.0f),2:F} (Should be 6.33)");
+                //Console.WriteLine($"Integral Function #1                 : {polynomialFunctionZero.Integral(0.0f, 2.0f),2:F} (Should be 3.96)");
+
+                Console.WriteLine($"Derivative Function #0               : {exponentialfunctionZero.Derivative(2.25f),2:F} (Should be 9.49)");
+
+                //Console.WriteLine($"Polynomial Derivative #1 (formula)   : {polynomialFunctionOne.Derivative(2.25f),2:F} (Should be 562616.29)");
+                //Console.WriteLine($"Polynomial Derivative #1 (numerical) : {NumericalMethods.Derivative(polynomialFunctionOne, 2.25f),2:F} (Should be 562616.40)");
+
+
             }
-            catch (ArgumentException)
-            {
-                TestMsg("Exception has been thrown");
-            }
-
-            Console.WriteLine("\nTest Reciprocal()");
-
-            frac = new Fraction(1, 2);
-            Test($"{frac.Reciprocal()}", "[2]");
-
-            frac = new Fraction(2, 3);
-            Test($"{frac.Reciprocal()}", "[1 1/2]");
-
-            frac = new Fraction(7, 14);
-            Test($"{frac.Reciprocal()}", "[2]");
-
-            try
-            {
-                frac = new Fraction(-2, 4);
-                Test($"{frac.Reciprocal()}", "-[2]");
-            }
-            catch (ArgumentException)
-            {
-                TestMsg("Exception has been thrown", false);
-            }
-
-            try
-            {
-                frac = new Fraction(-1, 3);
-                Test($"{frac.Reciprocal()}", "-[3]");
-            }
-            catch (ArgumentException)
-            {
-                TestMsg("Exception has been thrown", false);
-            }
-
-#endif
-#if STAGE2
-
-            Console.WriteLine("\nSTAGE 2 - Converters and arithmetic operators");
-
-            Console.WriteLine("\nTest converters");
-
-            frac = 5;
-            Test($"{frac}", "[5]");
-
-            frac = new Fraction(-6, 4);
-            double d = (double)frac;
-            Test($"{d.ToString(CultureInfo.InvariantCulture)}", "-1.5");
-
-            frac = new Fraction(4, 2);
-            long l = (long)frac;
-            Test($"{l}", "2");
-
-            frac = new Fraction(4, 3);
-            l = (long)frac;
-            Test($"{l}", "1");
-
-            Console.WriteLine("\nTest arithmetic operators");
-
-            Fraction u1, u2;
-
-            u1 = new Fraction(1, 2);
-            u2 = new Fraction(2, 3);
-
-            frac = u1 + u2;
-            Test($"{frac}", "[1 1/6]");
-
-            frac = u2 - u1;
-            Test($"{frac}", "[1/6]");
-
-            frac = -frac;
-            Test($"{frac}", "-[1/6]");
-
-            u1 = new Fraction(1, 6);
-            u2 = new Fraction(1, 5);
-            frac = u1 * u2;
-            Test($"{frac}", "[1/30]");
-
-            frac = u1 / u2;
-            Test($"{frac}", "[5/6]");
-
-#endif
-#if STAGE3
-
-            Console.WriteLine("\nSTAGE 3 - Comparison operators");
-
-            u1 = new Fraction(1, 2);
-            u2 = new Fraction(1, 3);
-            Test(u1 < u2, false);
-            Test(u1 == u2, false);
-            Test(u1 != u2, true);
-            Test(u1 >= u2, true);
-
-            u1 = new Fraction(4, 5);
-            u2 = new Fraction(1, 2);
-            Test(u1 > u2, true);
-            Test(u1 == u2, false);
-            Test(u1 != u2, true);
-            Test(u1 <= u2, false);
-
-            u1 = new Fraction(1, 2);
-            u2 = new Fraction(2, 4);
-            Test(u1 == u2, true);
-            Test(u1 != u2, false);
-            Test(u1.Equals(u2), true);
-            Test(u1.GetHashCode(), 3);
-            Test(u2.GetHashCode(), 3);
-
-#endif
-#if STAGE4
-
-            Console.WriteLine("\nSTAGE 4 - Hard tests (operations on large numbers)");
-
-            u1 = new Fraction(1, 30000000000);
-            u2 = new Fraction(1, 2);
-            u2 = u2 - u1;
-            frac = u1 + u2;
-            Test($"{frac}", "[1/2]");
-
-            u1 = new Fraction(1234567890, 7777777773);
-            u2 = u1;
-            u1 = 42 * u1;
-            frac = u1 / u2;
-            Test($"{frac}", "[42]");
-
-            u1 = new Fraction(1234567891011, 235690235690217);
-            u2 = new Fraction(11223344556677, 176366841573);
-            frac = u1 * u2;
-            Test($"{frac}", "[1/3]");
-
-            u1 = new Fraction(1234567891011, 235690235690217);
-            u2 = new Fraction(176366841573, 11223344556677);
-            frac = u2 / u1;
-            Test($"{frac}", "[3]");
-
-            u1 = new Fraction(5, 2222222222222222222);
-            u2 = new Fraction(5, 4444444444444444444);
-            Test(u1 > u2, true);
-            Test(u1 == u2, false);
-            Test(u2 <= u1, true);
-
-            u1 = new Fraction(1, 9000000000000000000);
-            u2 = new Fraction(1, 9000000000000000001);
-            Test(u1 < u2, false);
-            Test(u1 == u2, false);
-            Test(u1 >= u2, true);
-
-#endif
         }
     }
 }
+
